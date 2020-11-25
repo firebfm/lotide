c34941743ee036b0b38b354633c3872ec2c83196
Simon Huang <firebfm@gmail.com>
2020-11-25 22:47:30 +0000

    if object has array, check for match

--- eqObjects.js
+++ eqObjects.js
@@ -4,8 +4,13 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
+
   for (key in object1) {
-    if (object1[key] !== object2[key]) {
+    if (object1[key].isArray === object2[key].isArray) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
