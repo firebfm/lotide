af540d3636ceb1971cc51c75e6ca7acf3796aa49
Simon Huang <firebfm@gmail.com>
2020-11-30 18:59:20 +0000

    small typo

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,20 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns '5' for ['5,6,7']", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+});
+
+// how to test above mocha: npm test test/headTest.js
+// deleted old test code that did not use chai's assert.strictEqual
