55ebe029ec827a15ef01f02aa0811461e92f8a40
Simon Huang <firebfm@gmail.com>
2020-11-30 17:41:38 +0000

    test code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hi', 'hi');
+assertEqual('mom', 'dad');
+assertEqual(1, 10);
