ac92dbca3c60d36c51b44fa4932ca2a15748b54e
Simon Huang <firebfm@gmail.com>
2020-11-30 18:32:52 +0000

    refactor require, exports

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length != arr2.length) {
-    return false;
-  } else {
-    for (let i in arr1) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`🍈️ Assertion Passed: ${arr1} is ${arr2}`)
-  } else {
-    console.log(`🛑️ Assertion Failed: ${arr1} is not ${arr2}`);
-  }
-}
-
 const middle = function(arr) {
   let mymid = [];
   if (arr.length <= 2) {
@@ -34,13 +13,4 @@ const middle = function(arr) {
   return mymid;
 }
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // [2]
-console.log(middle([1, 2, 3, 4, 5])) // [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // [3,4]
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
